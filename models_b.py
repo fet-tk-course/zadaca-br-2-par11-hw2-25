36e70069ae63bb931c0015943e4331f94fa1da10
Danijela <danijela.mihajlovic@fet.ba>
2026-05-10 21:51:03 +0200

    Definisan model u models_b.py

--- models_b.py
+++ models_b.py
@@ -1,5 +1,27 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
 
-# TODO: Student B - Definiši svoj SQLModel entitet ovdje
-# 
+class Adopter(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    first_name: str
+    last_name: str
+    age: int
+    email: str
+    phone_number: str
+    has_experience: bool = False
+
+class AdopterCreate(SQLModel):
+    first_name: str
+    last_name: str
+    age: int
+    email: str
+    phone_number: str
+    has_experience: bool = False
+
+class AdopterUpdate(SQLModel):
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    age: Optional[int] = None
+    email: Optional[str] = None
+    phone_number: Optional[str] = None
+    has_experience: Optional[bool] = None
