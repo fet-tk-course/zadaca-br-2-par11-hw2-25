814ee80e9d387931edf7b9dbf1ca3d7d540b5ad0
Danijela <danijela.mihajlovic@fet.ba>
2026-05-11 22:49:50 +0200

    dodane get i put metode po id u routes_b.py

--- routes_b.py
+++ routes_b.py
@@ -21,3 +21,27 @@ def read_adopters(experience: Optional[bool] = None, session: Session = Depends(
     if experience is not None:
         statement = statement.where(Adopter.has_experience == experience)
     return session.exec(statement).all()
+
+@router.get("/{adopter_id}", response_model=Adopter)
+def read_adopter(adopter_id: int, session: Session = Depends(get_session)):
+    adopter = session.get(Adopter, adopter_id)
+    if not adopter:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Udomitelj nije pronađen")
+    return adopter
+
+@router.put("/{adopter_id}", response_model=Adopter)
+def replace_adopter(adopter_id: int, adopter_update: AdopterCreate, session: Session = Depends(get_session)):
+    adopter = session.get(Adopter, adopter_id)
+    if not adopter:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Udomitelj nije pronađen")
+    
+    adopter_data = adopter_update.model_dump()
+    for key, value in adopter_data.items():
+        setattr(adopter, key, value)
+    
+    session.add(adopter)
+    session.commit()
+    session.refresh(adopter)
+    return adopter
+
+
