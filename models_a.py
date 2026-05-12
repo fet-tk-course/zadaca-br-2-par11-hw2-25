915f82e91011873da56d1086283766118d51fde9
Dzana Dugonjic <dzana.dugonjic@fet.ba>
2026-05-12 20:32:15 +0200

    Add Animal model and schemas

--- models_a.py
+++ models_a.py
@@ -1,5 +1,29 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
 
-# TODO: Student A - Definiši svoj SQLModel entitet ovdje
-# 
+class Animal(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    species: str
+    age: int
+    vaccinated: bool
+    adoption_fee: float
+    description: Optional[str] = None
+
+
+class AnimalCreate(SQLModel):
+    name: str
+    species: str
+    age: int
+    vaccinated: bool
+    adoption_fee: float
+    description: Optional[str] = None
+
+
+class AnimalUpdate(SQLModel):
+    name: Optional[str] = None
+    species: Optional[str] = None
+    age: Optional[int] = None
+    vaccinated: Optional[bool] = None
+    adoption_fee: Optional[float] = None
+    description: Optional[str] = None
