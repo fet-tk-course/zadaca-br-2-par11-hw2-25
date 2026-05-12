cba1b525a4b1232be8a7443f18ede3f9fd802331
Dzana Dugonjic <dzana.dugonjic@fet.ba>
2026-05-12 20:33:49 +0200

    Implement CRUD endpoints for animals

--- routes_a.py
+++ routes_a.py
@@ -4,3 +4,10 @@ from sqlmodel import Session, select
 from database import get_session
 
 router = APIRouter(prefix="/resursi_a", tags=["Resurs A"])
+
+GET /animals
+GET /animals/{animal_id}
+POST /animals
+PUT /animals/{animal_id}
+PATCH /animals/{animal_id}
+DELETE /animals/{animal_id}
